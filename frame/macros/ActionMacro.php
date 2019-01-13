ea480c14193b91f89be8dc0338f670a92ff83cf7
Jed Dark <dartsidiys777@mail.ru>
2019-01-13 20:27:07 +0200

    Интегрирует механизм конфиг-валидации экшнов в ActionMacro.

--- frame/macros/ActionMacro.php
+++ frame/macros/ActionMacro.php
@@ -2,6 +2,7 @@
 
 use frame\Action;
 use frame\Core;
+use frame\tools\Json;
 
 use function lightlib\http_parse_query;
 
@@ -13,6 +14,19 @@ class ActionMacro extends Macro
         $name = explode('_', $args['action']);
         $id = $name[0];
         $class = $name[1];
-        (Action::$_current = $class::instance($args, $id))->exec();
+        $action = Action::$_current = $class::instance($args, $id);
+
+        $ruleDir = Core::$app->config->{'actions.validationConfigFolder'};
+        if ($ruleDir) {
+            $class = get_class($action);
+            $classPath = str_replace('\\', '/', $class);
+            $configFile = $ruleDir . '/' . $classPath . '.json';
+            if (file_exists($configFile)) {
+                $config = new Json($configFile);
+                $action->setValidationConfig($config);
+            }
+        }
+
+        $action->exec();
     }
 }
