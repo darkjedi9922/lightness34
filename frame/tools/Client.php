cd4075e48a42077dfdc166e691932af593bc3d0a
Jed Dark <darkjedi9922@gmail.com>
2019-06-25 12:20:57 +0300

    Make client id more unique.

--- frame/tools/Client.php
+++ frame/tools/Client.php
@@ -9,22 +9,28 @@ class Client
     {
         return $_SERVER['REMOTE_ADDR'];
     }
+    
     public static function getSessionId(): string
     {
         session_start_once();
         return session_id();
     }
+
+    /**
+     * Не стоит использовать этот id в целях безопасности, он предназначен лишь для
+     * идентификации клиентов.
+     */
     public static function getId(): string
     {
-        $cookies = new CookieTransmitter(60*60*24*7*365);
+        $cookies = new CookieTransmitter(60*60*24*7*365*10);
         if (!$cookies->isSetData('cid')) {
-            srand(time());
-            $id = rand();
-            $cookies->setData('cid', $id);
-            return $id;
+            $newId = md5(self::getSessionId() . time());
+            $cookies->setData('cid', $newId);
+            return $newId;
         }
         return $cookies->cid;
     }
+    
     public static function getUserAgent(): string
     {
         return $_SERVER['HTTP_USER_AGENT'];
