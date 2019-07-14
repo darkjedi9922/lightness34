bca723969d2198bd11c6257ae878c8098cdb87d0
Jed Dark <darkjedi9922@gmail.com>
2019-07-14 10:36:54 +0300

    Implement rules and route rules by generators (action is broken now).

--- frame/rules/RouteRules.php
+++ frame/rules/RouteRules.php
@@ -2,71 +2,26 @@
 
 use frame\route\Router;
 use frame\errors\HttpError;
+use frame\rules\RuleResult;
 
 class RouteRules extends Rules
 {
-    private $values;
-    private $rules;
-    private $interData = [];
-
     public function __construct(Router $router, array $rules)
     {
+        parent::__construct([], $rules);
         $this->setRouter($router);
-        $this->rules = $rules;
     }
 
     public function setRouter(Router $router)
     {
-        $this->values = $router->args;
-    }
-
-    public function getValues(): array
-    {
-        return $this->values;
+        $this->setValues($router->args);
     }
 
     public function assert()
     {
-        $this->interData = [];
-
-        // Проходимся по каждому полю
-        foreach ($this->rules as $field => $rules) {
-            $this->interData[$field] = [];
-
-            // Правил может не быть.
-            if (!isset($rules['rules'])) continue;
-
-            $value = $this->values[$field] ?? null;
-            $result = new RuleResult;
-
-            // Проходимся по каждому правилу проверок поля
-            foreach ($rules['rules'] as $rule => $ruleValue) {
-                $check = $this->getRuleCallback($rule);
-
-                // Т.к. для всей цепочки проверок правила используется один и тот
-                // же экземпляр класса, перед каждой обработкой необходимо
-                // восстанавливать результат после предыдущей обработки.
-                $result->restoreResult();
-                /** @var RuleResult|null $result */
-                $result = $check($ruleValue, $value, $result);
-
-                // Каждая проверка должна вернуть результат с одним из двух
-                // состояний: провал и успех.
-                if (!$result || !$result->hasResult())
-                    throw new RuleRuntimeException($this, $field, $rule,
-                        'Rule result state has not changed.');
-
-                if ($result->isFail()) throw new HttpError(HttpError::NOT_FOUND);
-                if ($result->isStopped()) break;
-            }
-
-            $this->interData[$field] = $result->getInterDataAll();
+        foreach ($this->getValidation() as $result) {
+            /** @var RuleResult $result */
+            if ($result->isFail()) throw new HttpError(HttpError::NOT_FOUND);
         }
-    }
-
-    /** @return mixed|null */
-    public function getInterData(string $field, string $data)
-    {
-        return $this->interData[$field][$data] ?? null;
     }
 }
--- frame/rules/Rules.php
+++ frame/rules/Rules.php
@@ -8,12 +8,42 @@ class Rules
 
     /** @var array Ассоциативный массив вида [string => callable] */
     private $ruleCallbacks = [];
+    private $values;
+    private $rules;
+    private $errors = [];
+    private $interData = [];
 
     public static function loadRule(string $rule): ?callable
     {
         $file = self::RULE_DIR . '/' . $rule . '.php';
         if (file_exists($file)) return require($file);
         return null;
+    }
+
+    public function __construct(array $values = [], array $rules = [])
+    {
+        $this->values = $values;
+        $this->rules = $rules;
+    }
+
+    public function setValues(array $values)
+    {
+        $this->values = $values;
+    }
+
+    public function getValues(): array
+    {
+        return $this->values;
+    }
+
+    public function setRules(array $rules)
+    {
+        $this->rules = $rules;
+    }
+
+    public function getRules(): array
+    {
+        return $this->rules;
     }
 
     /**
@@ -50,5 +80,74 @@ class Rules
         $callback = self::loadRule($rule);
         if (!$callback) throw new NoRuleException($rule);
         return $callback;
+    }
+
+    public function validate()
+    {
+        foreach ($this->getValidation() as $rule);
+    }
+
+    /**
+     * Во время прохода генератора полностью выполняется валидация правил.
+     * Значением генератора является RuleResult после каждой проверки каждого
+     * правила. Это можно использовать для "декорирования" валидации в специфических
+     * случаях.
+     */
+    public function getValidation(): \Generator
+    {
+        $this->errors = [];
+        $this->interData = [];
+
+        // Проходимся по каждому полю
+        foreach ($this->rules as $field => $rules) {
+            $this->interData[$field] = [];
+
+            // Правил может не быть.
+            if (!isset($rules['rules'])) continue;
+
+            $value = $this->values[$field] ?? null;
+            $result = new RuleResult;
+
+            // Проходимся по каждому правилу проверок поля
+            foreach ($rules['rules'] as $rule => $ruleValue) {
+                $check = $this->getRuleCallback($rule);
+
+                // Т.к. для всей цепочки проверок правила используется один и тот
+                // же экземпляр класса, перед каждой обработкой необходимо
+                // восстанавливать результат после предыдущей обработки.
+                $result->restoreResult();
+                /** @var RuleResult|null $result */
+                $result = $check($ruleValue, $value, $result);
+
+                // Каждая проверка должна вернуть результат с одним из двух
+                // состояний: провал и успех.
+                if (!$result || !$result->hasResult())
+                    throw new RuleRuntimeException(
+                        $this,
+                        $field,
+                        $rule,
+                        'Rule result state has not changed.'
+                    );
+
+                yield $result;
+
+                if ($result->isFail()) {
+                    if (!isset($this->errors[$field])) $this->errors[$field] = [];
+                    // Вместо int-кода ошибки, добавляем имя правила. Это лучше для 
+                    // читаемости и в целом красиво. Пока что нет необходимости 
+                    // оптимизировать это, чтобы хранить числа вместо строк.
+                    $this->errors[$field][] = $rule;
+                }
+                if ($result->isStopped()) break;
+            }
+
+            $this->interData[$field] = $result->getInterDataAll();
+        }
+    }
+
+    /** @return mixed|null */
+    public function getInterData(string $field, string $data)
+    {
+        return $this->interData[$field][$data] ?? null;
     }
 }
