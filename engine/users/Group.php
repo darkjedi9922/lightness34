9d8bd7931288525847795a99847bec73a98d3c5e
Jed Dark <darkjedi9922@gmail.com>
2019-10-28 16:49:33 +0200

    Add new system group.

--- engine/users/Group.php
+++ engine/users/Group.php
@@ -5,10 +5,18 @@ use frame\database\Identity;
 class Group extends Identity
 {
     const GUEST_ID = 1;
+    const USER_ID = 2;
     const ROOT_ID = 6;
 
     public static function getTable(): string
     {
         return 'user_groups';
+    }
+
+    public function isSystem(): bool
+    {
+        return $this->id === static::GUEST_ID
+            || $this->id === static::USER_ID
+            || $this->id === static::ROOT_ID;
     }
 }
