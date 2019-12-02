9259eee098eba4915e1e68cfc410f992d627eeab
Jed Dark <darkjedi9922@gmail.com>
2019-12-02 15:54:45 +0200

    Fix timeout message in admin enter page when user comes from no admin page.

--- views/pages/admin.php
+++ views/pages/admin.php
@@ -1,5 +1,6 @@
 <?php /** @var frame\views\Page $self */
 
+use cash\prev_router;
 use engine\admin\actions\LoginAction;
 use engine\admin\Auth;
 
@@ -8,12 +9,14 @@ $self->setMeta('admin-login-page-flag', true);
 
 $action = new LoginAction;
 $auth = new Auth;
+$prevRoute = prev_router::get();
+$isTimeup = $auth->isTimeup() && $prevRoute && $prevRoute->getPathPart(0) == 'admin'; 
 ?>
 
 <div class="centered-wrapper">
     <div class="box box--login">
         <center><h2>Вход в админ-панель</h2></center>
-        <?php if ($auth->isTimeup()): ?>
+        <?php if ($isTimeup): ?>
             <span style="font-weight:bold;color:red">Время сессии вышло</span><br><br>
         <?php endif?>
         <?php if ($action->hasError($action::E_WRONG_PASSWORD)): ?>
