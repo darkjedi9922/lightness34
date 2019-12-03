1ff6dab9001d6a0f0a7cca9719e74dc7f4aefa75
Jed Dark <darkjedi9922@gmail.com>
2019-12-03 09:23:03 +0200

    Fix headbar icons position with flex.

--- views/blocks/admin/headbar.php
+++ views/blocks/admin/headbar.php
@@ -14,17 +14,27 @@ $logTracker = new FileReadingTracker('log.txt', $me->id);
 $logUnreadedLines = $logTracker->countNewLines();
 ?>
 
-<a href="/" class="site" target="_blank"><i class="fontello icon-television"></i></a>
-<?php if ($group->id !== $group::GUEST_ID): ?>
-	<div class="mini-profile">
-		<?php if ($unreadedDialogs === 0): ?><a class="messages" href="/profile/dialogs" target="_blank"><i class="fontello icon-email"></i></a>
-		<?php else: ?><a class="messages new" href="/profile/dialogs" target="_blank"><i class="fontello icon-email"></i><span class="amount"> <?= $unreadedDialogs ?></span></a>
-		<?php endif ?>
-		<img class="avatar" src='/<?=$me->getAvatarUrl()?>'>
-		<a class="login" href='/profile?login=<?=$me->login?>' target="_blank"><?= $me->login ?></a>
-	</div>
-<?php endif ?>
-<div class="notice-bar">
-    <?php if($unreadedArticles !== 0):?><a href="/admin/new/articles"><i class="fontello icon-rss"></i> <?= $unreadedArticles ?></a><?php endif?>
-    <?php if($logUnreadedLines !== 0):?><a href="/admin/log"><i class="fontello icon-attention"></i> <?= $logUnreadedLines ?></a><?php endif?>
+<div class="head-bar__left">
+    <a href="/" class="site" target="_blank"><i class="fontello icon-television"></i></a>
+</div>
+<div class="head-bar__right">
+    <div class="notice-bar">
+        <?php if ($unreadedArticles !== 0) : ?>
+            <a href="/admin/new/articles"><i class="fontello icon-rss"></i> <?= $unreadedArticles ?></a>
+        <?php endif ?>
+        <?php if ($logUnreadedLines !== 0) : ?>
+            <a href="/admin/log"><i class="fontello icon-attention"></i> <?= $logUnreadedLines ?></a>
+        <?php endif ?>
+    </div>
+    <?php if ($group->id !== $group::GUEST_ID) : ?>
+        <div class="mini-profile">
+            <?php if ($unreadedDialogs === 0) : ?>
+                <a class="messages" href="/profile/dialogs" target="_blank"><i class="fontello icon-email"></i></a>
+            <?php else : ?>
+                <a class="messages new" href="/profile/dialogs" target="_blank"><i class="fontello icon-email"></i><span class="amount"> <?= $unreadedDialogs ?></span></a>
+            <?php endif ?>
+            <img class="avatar" src='/<?= $me->getAvatarUrl() ?>'>
+            <a class="login" href='/profile?login=<?= $me->login ?>' target="_blank"><?= $me->login ?></a>
+        </div>
+    <?php endif ?>
 </div>
