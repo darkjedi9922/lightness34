9481459776a64c5a2124c43e8f1ef7a2f15752ef
Jed Dark <darkjedi9922@gmail.com>
2019-12-05 10:10:18 +0200

    Add an admin settings page in the admin panel.

---
+++ views/pages/admin/settings/admin.php
@@ -0,0 +1,30 @@
+<?php /** @var frame\views\Page $self */
+
+use frame\tools\Init;
+use engine\users\Group;
+use frame\cash\config;
+use frame\actions\ViewAction;
+use engine\admin\actions\EditConfigAction;
+
+Init::accessGroup(Group::ROOT_ID);
+
+$config = config::get('admin');
+$edit = new ViewAction(EditConfigAction::class, ['name' => 'admin']);
+
+$self->setLayout('admin');
+?>
+
+<div class="box">
+    <form action="<?= $edit->getUrl() ?>" method="post">
+        <table>
+            <tr>
+                <td>Пароль:</td>
+                <td>
+                    <input name="password" type="password" 
+                        value="<?= $edit->getPost('password', $config->{'password'}) ?>">
+                </td>
+            </tr>
+        </table>
+        <button>Сохранить</button>
+    </form>
+</div>
