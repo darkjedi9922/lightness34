c5e94a1390d63c577c9565f1868d5348f5ef8622
Jed Dark <darkjedi9922@gmail.com>
2019-12-08 09:58:32 +0200

    Make some changes in styles of buttons and notices of admin panel.

--- views/pagers/admin.php
+++ views/pagers/admin.php
@@ -7,15 +7,12 @@ $next = $pager->getNext();
 $last = $pager->getLast();
 ?>
 
-<center>
-	<br/>
-	<div class="pager">
-		<?php if ($current > 2): ?><a href="<?= $pager->toLink('p', 1) ?>">1</a><?php endif ?>
-		<?php if ($current > 3): ?><span class="spacing">...</span><?php endif ?>
-		<?php if ($previous): ?><a href="<?= $this->toLink($previous) ?>"><?= $previous ?></a><?php endif ?>
-		<a class="current"><?= $current ?></a>
-		<?php if ($next): ?><a href="<?= $this->toLink($next) ?>"><?= $next ?></a><?php endif ?>
-		<?php if ($last - $current > 2): ?><span class="spacing">...</span><?php endif ?>
-		<?php if ($last - $current > 1): ?><a href="<?= $this->toLink($last) ?>"><?= $last ?></a><?php endif ?>
-	</div>
-</center>
+<div class="pager">
+    <?php if ($current > 2): ?><a class="pager__item" href="<?= $pager->toLink('p', 1) ?>">1</a><?php endif ?>
+    <?php if ($current > 3): ?><span class="pager__spacing">...</span><?php endif ?>
+    <?php if ($previous): ?><a class="pager__item" href="<?= $this->toLink($previous) ?>"><?= $previous ?></a><?php endif ?>
+    <a class="pager__item pager__item--current"><?= $current ?></a>
+    <?php if ($next): ?><a class="pager__item" href="<?= $this->toLink($next) ?>"><?= $next ?></a><?php endif ?>
+    <?php if ($last - $current > 2): ?><span class="pager__spacing">...</span><?php endif ?>
+    <?php if ($last - $current > 1): ?><a class="pager__item" href="<?= $this->toLink($last) ?>"><?= $last ?></a><?php endif ?>
+</div>
