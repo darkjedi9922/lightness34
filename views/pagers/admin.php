dd1d08cd4dfaee2fcbea6f468efc9a780fdcaa72
Jed Dark <darkjedi9922@gmail.com>
2019-12-13 18:34:59 +0200

    Fix admin pager.

--- views/pagers/admin.php
+++ views/pagers/admin.php
@@ -10,9 +10,9 @@ $last = $pager->getLast();
 <div class="pager">
     <?php if ($current > 2): ?><a class="pager__item" href="<?= $pager->toLink('p', 1) ?>">1</a><?php endif ?>
     <?php if ($current > 3): ?><span class="pager__spacing">...</span><?php endif ?>
-    <?php if ($previous): ?><a class="pager__item" href="<?= $this->toLink($previous) ?>"><?= $previous ?></a><?php endif ?>
+    <?php if ($previous): ?><a class="pager__item" href="<?= $pager->toLink('p', $previous) ?>"><?= $previous ?></a><?php endif ?>
     <a class="pager__item pager__item--current"><?= $current ?></a>
-    <?php if ($next): ?><a class="pager__item" href="<?= $this->toLink($next) ?>"><?= $next ?></a><?php endif ?>
+    <?php if ($next): ?><a class="pager__item" href="<?= $pager->toLink('p', $next) ?>"><?= $next ?></a><?php endif ?>
     <?php if ($last - $current > 2): ?><span class="pager__spacing">...</span><?php endif ?>
-    <?php if ($last - $current > 1): ?><a class="pager__item" href="<?= $this->toLink($last) ?>"><?= $last ?></a><?php endif ?>
+    <?php if ($last - $current > 1): ?><a class="pager__item" href="<?= $pager->toLink('p', $last) ?>"><?= $last ?></a><?php endif ?>
 </div>
