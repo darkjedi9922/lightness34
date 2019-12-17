ca337bb42d57dc354de0aebe32db7b4fce594101
Jed Dark <darkjedi9922@gmail.com>
2019-12-17 21:48:58 +0200

    Add get params to route statistics page.

--- views/pages/admin/statistics.php
+++ views/pages/admin/statistics.php
@@ -23,15 +23,19 @@ $self->setLayout('admin');
         <?php foreach ($routes as $route) : /** @var RouteStat $route */ ?>
             <?php 
                 $router = new Router($route->url);
-                $severity = 'warning';
-                switch ((int) $route->code / 100) {
-                    case 2: $severity = 'ok'; break;
+                $severity = 'ok';
+                switch ((int) ($route->code / 100)) {
+                    case 4: $severity = 'warning'; break;
                     case 5: $severity = 'error'; break;
                 }
+                $empty = $router->pagename === '';
+                $pagename = !$empty ? $router->pagename : 'index request';
             ?>
             <tr class="routes__mainrow">
                 <td class="routes__path">
-                    <span class="routes__pagename"><?= $router->pagename ?></span>
+                    <span class="routes__pagename <?= $empty ? 'routes__pagename--index' : '' ?>">
+                        <?= $pagename ?>
+                    </span>
                     <?php if ($route->ajax): ?>
                         <span class="routes__mark routes__mark--ajax">ajax</span>
                     <?php endif  ?>
@@ -42,11 +46,22 @@ $self->setLayout('admin');
                 <td class="routes__code routes__code--<?= $severity ?>"><?= $route->code ?></td>
                 <td class="routes__time"><?= date('d.m.Y H:i', $route->time) ?></td>
             </tr>
-            <tr class="routes__subrow">
-                <td colspan="100" class="route__details">
-
-                </td>
-            </tr>
+            <?php if (!empty($router->args)): ?>
+                <tr class="routes__subrow">
+                    <td colspan="100" class="routes__details">
+                        <span class="routes__subheader">Get</span>
+                        <?php foreach ($router->args as $key => $value): ?>
+                            <div class="param">
+                                <span class="routes__param-name"><?= $key ?></span>
+                                <span class="routes__param-equals">=</span>
+                                <span class="class routes__param-value
+                                    <?= $value === '' ? 'routes__param-value--empty' : '' ?>"
+                                ><?= $value !== '' ? $value : 'empty' ?></span>
+                            </div>
+                        <?php endforeach ?>
+                    </td>
+                </tr>
+            <?php endif ?>
         <?php endforeach ?>
     </table>
 </div>
