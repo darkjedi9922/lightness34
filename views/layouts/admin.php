5223c6911c2f24ec4d3c40ddbac4c37f207699ec
Jed Dark <darkjedi9922@gmail.com>
2019-12-18 20:37:30 +0200

    Change table markup of admin panel to flex.

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -5,12 +5,10 @@ use frame\views\Block;
 $self->setLayout('admin-base');
 ?>
 
-<table class="container">
-    <tr>
-        <td rowspan="2" class="sidebox"><?php (new Block('admin/left'))->show() ?></td>
-        <td class="head-bar"><?php (new Block('admin/headbar'))->show() ?></td>
-    </tr>
-    <tr>
-        <td class="content"><?php $self->showChild() ?></td>
-    </tr>
-</table>
+<div class="container">
+    <div rowspan="2" class="container__sidebox"><?php (new Block('admin/left'))->show() ?></div>
+    <div class="container__body">
+        <div class="container__head-bar head-bar"><?php (new Block('admin/headbar'))->show() ?></div>
+        <div class="container__content content"><?php $self->showChild() ?></div>
+    </div>
+</div>
