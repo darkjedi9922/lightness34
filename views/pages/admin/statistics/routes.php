769dec742ee1ecde17e9d12eed37506538999e60
Jed Dark <darkjedi9922@gmail.com>
2019-12-21 20:18:37 +0200

    Add breadcrumbs to routes page.

--- views/pages/admin/statistics/routes.php
+++ views/pages/admin/statistics/routes.php
@@ -15,7 +15,11 @@ $routes = new IdentityList(RouteStat::class, ['time' => 'DESC']);
 $self->setLayout('admin');
 ?>
 
-<span class="content__title">Маршрутизация</span>
+<div class="breadcrumbs">
+    <span class="breadcrumbs__item">Статистика</span>
+    <span class="breadcrumbs__divisor"></span>
+    <span class="breadcrumbs__item breadcrumbs__item--current">Маршруты</span>
+</div>
 <div class="box">
     <div class="routes">
         <div class="routes__headers">
