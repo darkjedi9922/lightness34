8b3cc4dffe0698b7dd6926e2363b10c4cdf5f36d
Jed Dark <darkjedi9922@gmail.com>
2019-12-22 18:14:43 +0200

    Markup routes with table style component.

--- views/pages/admin/statistics/routes.php
+++ views/pages/admin/statistics/routes.php
@@ -20,13 +20,13 @@ $self->setLayout('admin');
     <span class="breadcrumbs__divisor"></span>
     <span class="breadcrumbs__item breadcrumbs__item--current">Маршруты</span>
 </div>
-<div class="box">
-    <div class="routes">
-        <div class="routes__headers">
-            <span class="routes__header routes__cell routes__cell--path">Path</span>
-            <span class="routes__header routes__cell routes__cell--code">Code</span>
-            <span class="routes__header routes__cell routes__cell--time">Time</span>
-        </div>
+<div class="box box--table">
+    <table class="table routes">
+        <tr class="table__headers">
+            <td class="table__header">Path</td>
+            <td class="table__header">Code</td>
+            <td class="table__header">Time</td>
+        </tr>
         <?php foreach ($routes as $route) : /** @var RouteStat $route */ ?>
             <?php
             $router = new Router($route->url);
@@ -42,9 +42,9 @@ $self->setLayout('admin');
             $empty = $router->pagename === '';
             $pagename = !$empty ? $router->pagename : 'index request';
             ?>
-            <div class="routes__item">
-                <div class="routes__route">
-                    <span class="routes__cell routes__cell--path">
+            <tbody class="table__item-wrapper">
+                <tr class="table__item">
+                    <td class="table__cell">
                         <span class="routes__pagename <?= $empty ? 'routes__pagename--index' : '' ?>">
                             <?= $pagename ?>
                         </span>
@@ -57,66 +57,66 @@ $self->setLayout('admin');
                         <?php if ($route->type === $route::ROUTE_TYPE_DYNAMIC_PAGE) : ?>
                             <span class="routes__mark routes__mark--dynamic">dynamic</span>
                         <?php endif ?>
-                    </span>
-                    <div class="routes__cell routes__cell--code">
+                    </td>
+                    <td class="table__cell">
                         <span class="routes__code routes__code--<?= $severity ?>"><?= $route->code ?></span>
-                    </div>
-                    <div class="routes__cell routes__cell--time">
+                    </td>
+                    <td class="table__cell">
                         <span class="routes__time"><?= date('d.m.Y H:i', $route->time) ?></span>
-                    </div>
-                </div>
-                <div class="routes__details">
-                    <?php if ($route->viewfile) : ?>
-                        <div class="routes__get">
-                            <span class="routes__subheader">View file</span>
-                            <div class="param">
+                    </td>
+                </tr>
+                <tr class="table__item-details-wrapper">
+                    <td class="table__item-details" colspan="100">
+                        <?php if ($route->viewfile) : ?>
+                            <span class="table__subheader">View file</span>
+                            <div class="table__detail-wrapper">
                                 <span class="routes__param-value"><?= $route->viewfile ?></span>
                             </div>
-                        </div>
-                    <?php endif ?>
-                    <?php if ($route->type === $route::ROUTE_TYPE_DYNAMIC_PAGE) :
-                        $params = Records::select(DynamicRouteParam::getTable(), [
-                            'route_id' => $route->id
-                        ])->load();
-                        ?>
-                        <?php if ($params->count()) :
-                            $paramsIterator = new IdentityIterator(
-                                $params,
-                                DynamicRouteParam::class
-                            );
+                        <?php endif ?>
+                        <?php if ($route->type === $route::ROUTE_TYPE_DYNAMIC_PAGE) :
+                            $params = Records::select(DynamicRouteParam::getTable(), [
+                                'route_id' => $route->id
+                            ])->load();
                             ?>
-                            <div class="routes__get">
-                                <span class="routes__subheader">Dynamic Page Arguments</span>
-                                <?php foreach ($paramsIterator as $param) : /** @var DynamicRouteParam $param */ ?>
-                                    <div class="param">
-                                        <span class="routes__param-name"><?= $param->index ?></span>
+                            <?php if ($params->count()) :
+                                $paramsIterator = new IdentityIterator(
+                                    $params,
+                                    DynamicRouteParam::class
+                                );
+                                ?>
+                                <span class="table__subheader">Dynamic Page Arguments</span>
+                                <div class="table__detail-wrapper">
+                                    <?php foreach ($paramsIterator as $param) : /** @var DynamicRouteParam $param */ ?>
+                                        <div class="table__item-detail">
+                                            <span class="routes__param-name"><?= $param->index ?></span>
+                                            <span class="routes__param-equals">=</span>
+                                            <span class="routes__param-value"><?= $param->value ?></span>
+                                        </div>
+                                    </div>
+                                <?php endforeach ?>
+                            <?php endif ?>
+                        <?php endif ?>
+                        <?php if (!empty($router->args)) : ?>
+                            <span class="table__subheader">Get</span>
+                            <div class="table__detail-wrapper">
+                                <?php foreach ($router->args as $key => $value) : ?>
+                                    <div class="table__item-detail">
+                                        <span class="routes__param-name"><?= $key ?></span>
                                         <span class="routes__param-equals">=</span>
-                                        <span class="routes__param-value"><?= $param->value ?></span>
+                                        <span class="routes__param-value <?= $value === '' ? 'routes__param-value--empty' : '' ?>"><?= $value !== '' ? $value : 'empty' ?></span>
                                     </div>
                                 <?php endforeach ?>
                             </div>
                         <?php endif ?>
-                    <?php endif ?>
-                    <?php if (!empty($router->args)) : ?>
-                        <div class="routes__get">
-                            <span class="routes__subheader">Get</span>
-                            <?php foreach ($router->args as $key => $value) : ?>
-                                <div class="param">
-                                    <span class="routes__param-name"><?= $key ?></span>
-                                    <span class="routes__param-equals">=</span>
-                                    <span class="routes__param-value <?= $value === '' ? 'routes__param-value--empty' : '' ?>"><?= $value !== '' ? $value : 'empty' ?></span>
-                                </div>
-                            <?php endforeach ?>
-                        </div>
-                    <?php endif ?>
-                    <?php if ($route->code_info) : ?>
-                        <span class="routes__status routes__status--<?= $severity ?>">
-                            <span class="routes__status-code">Status <?= $route->code ?></span>
-                            <span class="routes__status-message"><?= $route->code_info ?></span>
-                        </span>
-                    <?php endif ?>
-                </div>
-            </div>
+                        <?php if ($route->code_info) : ?>
+                            <span class="routes__status routes__status--<?= $severity ?>">
+                                <span class="routes__status-code">Status <?= $route->code ?></span>
+                                <span class="routes__status-message"><?= $route->code_info ?></span>
+                            </span>
+                        <?php endif ?>
+                    </td>
+                </tr>
+            </tbody>
         <?php endforeach ?>
-    </div>
+    </table>
 </div>
