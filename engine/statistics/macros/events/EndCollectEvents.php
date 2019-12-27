94c884b7e5359e0b275aa66d694c142e5fec4d6f
Jed Dark <darkjedi9922@gmail.com>
2019-12-27 18:52:14 +0200

    Limit events statistics.

--- engine/statistics/macros/events/EndCollectEvents.php
+++ engine/statistics/macros/events/EndCollectEvents.php
@@ -4,6 +4,8 @@ use engine\statistics\macros\BaseStatCollector;
 use engine\statistics\stats\EventRouteStat;
 use engine\statistics\stats\EventSubscriberStat;
 use engine\statistics\stats\EventEmitStat;
+use frame\cash\config;
+use frame\cash\database;
 
 class EndCollectEvents extends BaseStatCollector
 {
@@ -24,7 +26,13 @@ class EndCollectEvents extends BaseStatCollector
     protected function collect(...$args)
     {
         $routeId = $this->routeStat->insert();
-        
+        $this->insertSubscribers($routeId);
+        $this->insertEmits($routeId);
+        $this->deleteOldStats();
+    }
+
+    private function insertSubscribers(int $routeId)
+    {
         $subscribers = $this->subscriberCollector->getSubscribers();
         for ($i = 0, $c = count($subscribers); $i < $c; ++$i) {
             /** @var EventSubscriberStat $subscriber */
@@ -32,7 +40,10 @@ class EndCollectEvents extends BaseStatCollector
             $subscriber->route_id = $routeId;
             $subscriber->insert();
         }
+    }
 
+    private function insertEmits(int $routeId)
+    {
         $emits = $this->emitCollector->getEmits();
         for ($i = 0, $c = count($emits); $i < $c; ++$i) {
             /** @var EventEmitStat $emitStat */
@@ -40,5 +51,31 @@ class EndCollectEvents extends BaseStatCollector
             $emitStat->route_id = $routeId;
             $emitStat->insert();
         }
+    }
+
+    private function deleteOldStats()
+    {
+        $routeTable = EventRouteStat::getTable();
+        $subscribersTable = EventSubscriberStat::getTable();
+        $emitsTable = EventEmitStat::getTable();
+        $config = config::get('statistics');
+        $limit = $config->{'events.history.limit'};
+        database::get()->query(
+            "DELETE $routeTable, $subscribersTable, $emitsTable
+            FROM
+            (
+                (
+                    $routeTable LEFT OUTER JOIN $subscribersTable 
+                    ON $routeTable.id = $subscribersTable.route_id
+                )
+                LEFT OUTER JOIN $emitsTable
+                ON $routeTable.id = $emitsTable.route_id 
+            )
+            INNER JOIN 
+            (
+                SELECT id FROM $routeTable ORDER BY id DESC LIMIT $limit, 999999
+            ) AS cond_table
+            ON $routeTable.id = cond_table.id"
+        );
     }
 }
