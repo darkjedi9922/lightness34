22eabae76dbb12e952d2e3e1b49a1bc8c91dcb30
Jed Dark <darkjedi9922@gmail.com>
2019-12-29 19:53:04 +0200

    Fix numbers of items in the subscriber list.

--- public/scripts/admin/components/events/subscriber-list.tsx
+++ public/scripts/admin/components/events/subscriber-list.tsx
@@ -21,7 +21,9 @@ class SubscriberList extends React.Component<Props> {
             );
             items.unshift(
                 <div key={i} className="param">
-                    <span className="param__number">{i + 1}</span>
+                    <span className="param__number">
+                        {this.props.subscribers.length - i}
+                    </span>
                     <span className="param__name">{subscriber.event}</span>
                     <span className="param__value">{subscriber.class}</span>
                     &nbsp;
