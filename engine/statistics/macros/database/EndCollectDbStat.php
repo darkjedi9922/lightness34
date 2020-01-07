726fffa7054cf0c9097e6be9687322656b22c5bf
Jed Dark <darkjedi9922@gmail.com>
2020-01-07 18:27:27 +0200

    Limit database history statistics.

--- engine/statistics/macros/database/EndCollectDbStat.php
+++ engine/statistics/macros/database/EndCollectDbStat.php
@@ -2,6 +2,9 @@
 
 use engine\statistics\macros\BaseStatCollector;
 use engine\statistics\stats\QueryStat;
+use engine\statistics\stats\QueryRouteStat;
+use frame\cash\database;
+use frame\cash\config;
 
 class EndCollectDbStat extends BaseStatCollector
 {
@@ -19,10 +22,36 @@ class EndCollectDbStat extends BaseStatCollector
     protected function collect(...$args)
     {
         $routeId = $this->routeStatCollector->getRouteStat()->insert();
+        $this->insertQueryStats($routeId);
+        $this->deleteOldStats();
+    }
+
+    private function insertQueryStats(int $routeId)
+    {
         foreach ($this->startQueryCollector->getQueryStats() as $queryStat) {
             /** @var QueryStat $queryStat */
             $queryStat->route_id = $routeId;
             $queryStat->insert();
         }
+    }
+
+    private function deleteOldStats()
+    {
+        $routeTable = QueryRouteStat::getTable();
+        $queryTable = QueryStat::getTable();
+        $limit = config::get('statistics')->{'queries.history.limit'};
+        database::get()->query(
+            "DELETE $routeTable, $queryTable
+            FROM
+                $routeTable 
+                LEFT OUTER JOIN $queryTable 
+                    ON $routeTable.id = $queryTable.route_id
+                INNER JOIN
+                (
+                    SELECT id FROM $routeTable 
+                    ORDER BY id DESC LIMIT $limit, 999999
+                ) AS cond_table
+                    ON $routeTable.id = cond_table.id"
+        );
     }
 }
