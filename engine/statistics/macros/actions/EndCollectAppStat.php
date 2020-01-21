3ae563ff2b6517845ac349e5cad4709d79016fcc
Jed Dark <darkjedi9922@gmail.com>
2020-01-21 10:53:51 +0200

    Fix action response type collecting.

--- engine/statistics/macros/actions/EndCollectAppStat.php
+++ engine/statistics/macros/actions/EndCollectAppStat.php
@@ -24,12 +24,12 @@ class EndCollectAppStat extends BaseStatCollector
         if (!$this->stat->class) return;
 
         if (!$this->errorCollector->isExecuted()) {
-            if ($this->stat->ajax) {
-                $this->stat->response_type = ActionStat::RESPONSE_TYPE_JSON;
-                $this->stat->response_info = null;
-            } else if (Response::getUrl() !== null) {
+            if (Response::getUrl() !== null) {
                 $this->stat->response_type = ActionStat::RESPONSE_TYPE_REDIRECT;
                 $this->stat->response_info = Response::getUrl();
+            } else {
+                $this->stat->response_type = ActionStat::RESPONSE_TYPE_JSON;
+                $this->stat->response_info = null;
             }
         }
 
