27de7b9d23fa97c7e7d87160e2351f14580f765f
Jed Dark <darkjedi9922@gmail.com>
2020-01-22 20:26:55 +0200

    Fix default parameter values of the QueryException constructor.

--- frame/database/QueryException.php
+++ frame/database/QueryException.php
@@ -9,8 +9,8 @@ class QueryException extends \Exception
     public function __construct(
         string $query,
         string $message = '',
-        int $code = '',
-        Throwable $previous
+        int $code = 0,
+        ?Throwable $previous = null
     ) {
         parent::__construct($message, $code, $previous);
         $this->query = $query;
