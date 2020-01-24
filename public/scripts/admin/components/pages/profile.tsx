9c8f1814101da93386f4ad1b72a996f8e529b109
Jed Dark <darkjedi9922@gmail.com>
2020-01-24 09:24:34 +0200

    Move the profile page to a React component.

---
+++ public/scripts/admin/components/pages/profile.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import Breadcrumbs from '../common/Breadcrumbs';
+
+interface User {
+    id: number,
+    login: string,
+    hasAvatar: boolean,
+    avatarUrl: string,
+    name: string,
+    surname: string,
+    gender: string,
+    email: string,
+    group: string,
+    registrationDate: string,
+    lastOnlineTime?: string,
+    lastUserAgent: string,
+    isOnline: boolean
+}
+
+interface PageRights {
+    canEdit: boolean,
+    canChangeGroup: boolean
+}
+
+interface Actions {
+    deleteAvatarUrl: string
+}
+
+interface ProfileProps {
+    user: User,
+    rights: PageRights,
+    actions: Actions
+}
+
+class ProfilePage extends React.Component<ProfileProps> {
+    public render(): React.ReactNode {
+        return <>
+            <div className="content__header">
+                <Breadcrumbs items={[{
+                    name: "Пользователи",
+                    link: '/admin/users'
+                }, {
+                    name: `ID ${this.props.user.id}`
+                }]} />
+            </div>
+            <div className="box">
+                <div style={{
+                        float: 'left',
+                        marginRight: '1%',
+                        maxWidth: '40%'
+                }}>
+                    <img src={this.props.user.avatarUrl} style={{
+                        maxWidth: '100%'
+                    }} />
+                    {this.props.rights.canEdit && <>
+                        {this.props.user.hasAvatar && <>
+                            <br/>
+                            <a 
+                                className="link"
+                                href={this.props.actions.deleteAvatarUrl}
+                            >Удалить аватар</a>
+                        </>}
+                        <br/>
+                        <a
+                            className="link"
+                            href={`/admin/users/edit/profile?id=${this.props.user.id}`}
+                        >Редактировать профиль</a>
+                    </>}
+                </div>
+                <div>
+                    Логин: <b>{this.props.user.login}</b>
+                    {(this.props.user.name || this.props.user.surname) && <>
+                        <br/>Имя: {this.props.user.name} {this.props.user.surname}
+                    </>}
+                    <br/>
+                    Пол: {this.props.user.gender}
+                    {this.props.user.email && <>
+                        <br/>E-mail: {this.props.user.email}
+                    </>}
+                    <br/>
+                    Дата регистрации: {this.props.user.registrationDate}
+                    <br/>
+                    Последний раз онлайн: {this.props.user.lastOnlineTime
+                        ? this.props.user.lastOnlineTime
+                        : "Никогда"
+                    }
+                    <br/>
+                    Последнее устройство: {this.props.user.lastUserAgent}
+                    <br/>
+                    Группа: {this.props.user.group}
+                    {this.props.rights.canChangeGroup &&
+                        <a
+                            className="link"
+                            href={`/admin/users/change/group?id=${this.props.user.id}`}
+                        >[Изменить]</a>
+                    }
+                    <br/>
+                        Статус: {this.props.user.isOnline 
+                            ? <span style={{color: 'green'}}>Онлайн</span>
+                            : <span style={{color: 'red'}}>Оффлайн</span>
+                        }
+                </div>
+            </div>
+        </>;
+    }
+}
+
+export default ProfilePage;
--- views/pages/admin/users/$profile.php
+++ views/pages/admin/users/$profile.php
@@ -5,10 +5,10 @@ use frame\tools\Init;
 use engine\users\Gender;
 use engine\users\Group;
 use engine\users\cash\my_rights;
-use frame\cash\pagenumber;
 use engine\users\cash\user_me;
 use engine\users\actions\DeleteAvatarAction;
 use frame\actions\ViewAction;
+use frame\tools\JsonEncoder;
 
 Init::require(count($self->getArguments()) === 1);
 
@@ -20,46 +20,39 @@ Init::require($profile !== null);
 $gender = Gender::selectIdentity($profile->gender_id);
 $group = Group::selectIdentity($profile->group_id);
 $rights = my_rights::get('users');
-$prevPagenumber = pagenumber::get(true);
 $me = user_me::get();
 
 $deleteAvatar = new ViewAction(DeleteAvatarAction::class, ['uid' => $profile->id]);
+
+$pageProps = [
+    'user' => [
+        'id' => $profile->id,
+        'login' => $profile->login,
+        'hasAvatar' => $profile->hasAvatar(),
+        'avatarUrl' => '/' . $profile->getAvatarUrl(),
+        'name' => $profile->name,
+        'surname' => $profile->surname,
+        'gender' => $gender->name,
+        'email' => $profile->email,
+        'group' => $group->name,
+        'registrationDate' => $profile->registration_date,
+        'lastOnlineTime' => $profile->last_online_time
+            ? date('d.m.Y H:i', $profile->last_online_time)
+            : null,
+        'lastUserAgent' => $profile->last_user_agent,
+        'isOnline' => $profile->online === '1'
+    ],
+    'rights' => [
+        'canEdit' => $rights->canOneOf([
+            'edit-all' => [$profile],
+            'edit-own' => [$profile]
+        ]),
+        'canChangeGroup' => $me->group_id === Group::ROOT_ID 
+            && $group->id !== Group::ROOT_ID
+    ],
+    'actions' => $deleteAvatar->getUrl()
+];
+$pageProps = JsonEncoder::forHtmlAttribute($pageProps);
 ?>
 
-<div class="content__header">
-    <div class="breadcrumbs">
-        <a href="/admin/users?p=<?= $prevPagenumber ?>" class="breadcrumbs__item breadcrumbs__item--link">Пользователи</a>
-        <span class="breadcrumbs__divisor"></span>
-        <span class="breadcrumbs__item breadcrumbs__item--current">ID <?= $profile->id ?></span>
-    </div>
-</div>
-<div class="box">
-    <div style="float:left;margin-right:1%;max-width:40%">
-        <img src="/<?= $profile->getAvatarUrl() ?>" style="max-width:100%">
-        <?php if ($rights->canOneOf([
-            'edit-all' => [$profile], 
-            'edit-own' => [$profile]
-        ])) : ?>
-            <?php if ($profile->avatar) : ?><br><a class="link" href="<?= $deleteAvatar->getUrl() ?>">Удалить аватар</a><?php endif ?>
-            <br><a class="link" href="/admin/users/edit/profile?id=<?= $profile->id ?>">Редактировать профиль</a>
-        <?php endif ?>
-    </div>
-    <div>
-        Логин: <b><?= $profile->login ?></b>
-        <?php if ($profile->name || $profile->surname): ?>
-            <br />Имя: <?= $profile->name ?> <?= $profile->surname ?>
-        <?php endif ?>
-        <br />Пол: <?= $gender->name ?>
-        <?php if ($profile->email) echo '<br/>E-mail: ', $profile->email ?>
-        <br />Дата регистрации: <?= date('d.m.Y H:i', $profile->registration_date) ?>
-        <br />Последний раз онлайн: <?php if ($profile->last_online_time): ?><?= date('d.m.Y H:i', $profile->last_online_time) ?>
-        <?php else : ?>Никогда<?php endif ?>
-        <br>Последнее устройство: <?= $profile->last_user_agent ?>
-        <br />Группа: <?= $group->name ?>
-        <?php if ((int)$me->group_id === Group::ROOT_ID && $group->id !== Group::ROOT_ID) : ?>
-            <a class="link" href="/admin/users/change/group?id=<?= $profile->id ?>">[Изменить]</a>
-        <?php endif ?>
-        <br />Статус: <?php if ($profile->online === '1'): ?><span style="color:green">Онлайн</span>
-        <?php else: ?><span style="color:red">Оффлайн</span><?php endif ?>
-    </div>
-</div>
+<div id="profile-page" data-props="<?= $pageProps ?>"></div>
