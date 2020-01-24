aa6369c963efd8e79d2f8649c6ba55d24abf2422
Jed Dark <darkjedi9922@gmail.com>
2020-01-24 11:21:55 +0200

    Improve styles of profile page.

--- public/scripts/admin/components/pages/profile.tsx
+++ public/scripts/admin/components/pages/profile.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Breadcrumbs from '../common/Breadcrumbs';
+import Parameter from '../parameter';
+import classNames from 'classnames';
+import Status, { Type } from '../status';
+import Mark from '../mark';
 
 interface User {
     id: number,
@@ -34,74 +38,100 @@ interface ProfileProps {
 
 class ProfilePage extends React.Component<ProfileProps> {
     public render(): React.ReactNode {
-        return <>
-            <div className="content__header">
-                <Breadcrumbs items={[{
-                    name: "Пользователи",
-                    link: '/admin/users'
-                }, {
-                    name: `ID ${this.props.user.id}`
-                }]} />
-            </div>
-            <div className="box">
-                <div style={{
-                        float: 'left',
-                        marginRight: '1%',
-                        maxWidth: '40%'
-                }}>
-                    <img src={this.props.user.avatarUrl} style={{
-                        maxWidth: '100%'
-                    }} />
-                    {this.props.rights.canEdit && <>
-                        {this.props.user.hasAvatar && <>
-                            <br/>
-                            <a 
-                                className="link"
-                                href={this.props.actions.deleteAvatarUrl}
-                            >Удалить аватар</a>
-                        </>}
-                        <br/>
-                        <a
-                            className="link"
-                            href={`/admin/users/edit/profile?id=${this.props.user.id}`}
-                        >Редактировать профиль</a>
-                    </>}
-                </div>
-                <div>
-                    Логин: <b>{this.props.user.login}</b>
-                    {(this.props.user.name || this.props.user.surname) && <>
-                        <br/>Имя: {this.props.user.name} {this.props.user.surname}
-                    </>}
-                    <br/>
-                    Пол: {this.props.user.gender}
-                    {this.props.user.email && <>
-                        <br/>E-mail: {this.props.user.email}
-                    </>}
-                    <br/>
-                    Дата регистрации: {this.props.user.registrationDate}
-                    <br/>
-                    Последний раз онлайн: {this.props.user.lastOnlineTime
-                        ? this.props.user.lastOnlineTime
-                        : "Никогда"
-                    }
-                    <br/>
-                    Последнее устройство: {this.props.user.lastUserAgent}
-                    <br/>
-                    Группа: {this.props.user.group}
-                    {this.props.rights.canChangeGroup &&
-                        <a
-                            className="link"
-                            href={`/admin/users/change/group?id=${this.props.user.id}`}
-                        >[Изменить]</a>
-                    }
-                    <br/>
-                        Статус: {this.props.user.isOnline 
-                            ? <span style={{color: 'green'}}>Онлайн</span>
-                            : <span style={{color: 'red'}}>Оффлайн</span>
+        return (
+            <div className="profile">
+                <div className="content__header">
+                    <div className="breadcrumbs-wrapper">
+                        <Breadcrumbs items={[{
+                            name: "Пользователи",
+                            link: '/admin/users'
+                        }, {
+                            name: `ID ${this.props.user.id}`
+                        }]} />
+                        {this.props.user.isOnline
+                            ? <Mark
+                                className="profile__status"
+                                label="Online"
+                                color="green"
+                            />
+                            : <Mark
+                                className="profile__status"
+                                label="Offline"
+                                color="red"
+                            />
                         }
+                    </div>
+                    <div className="actions">
+                        {this.props.rights.canChangeGroup &&
+                            <a
+                                href={`/admin/users/change/group?id=${this.props.user.id}`}
+                                className="button actions__item"
+                            >
+                                <i className="button__icon icon-group"></i>
+                                Изменить группу
+                            </a>
+                        }
+                        {this.props.rights.canEdit && <>
+                            <a
+                                href={`/admin/users/edit/profile?id=${this.props.user.id}`}
+                                className="button actions__item"
+                            >
+                                <i className="button__icon icon-pencil"></i>
+                                Редактировать
+                            </a>
+                            {this.props.user.hasAvatar &&
+                                <a
+                                    className="button button--red actions__item"
+                                    href={this.props.actions.deleteAvatarUrl}
+                                >
+                                    <i className="button__icon icon-trash"></i>
+                                    Удалить аватар
+                                </a>
+                            }
+                        </>}
+                    </div>
+                </div>
+                <div className="box profile__box">
+                    <img src={this.props.user.avatarUrl} className="profile__avatar"/>
+                    <div className="profile__data">
+                        <Parameter 
+                            name="Логин"
+                            value={this.props.user.login}
+                        />
+                        <Parameter
+                            name="Имя"
+                            value={this.props.user.name}
+                        />
+                        <Parameter
+                            name="Фамилия"
+                            value={this.props.user.surname}
+                        />
+                        <Parameter 
+                            name="Пол"
+                            value={this.props.user.gender}
+                        />
+                        <Parameter
+                            name="Email"
+                            value={this.props.user.email}
+                        />
+                        <Parameter
+                            name="Дата регистрации"
+                            value={this.props.user.registrationDate}
+                        />
+                        <Parameter
+                            name="Последний раз онлайн"
+                            value={this.props.user.lastOnlineTime || 'Никогда'}/>
+                        <Parameter name="Последнее устройство"
+                            value={this.props.user.lastUserAgent}
+                        />
+                        <Parameter
+                            name="Группа"
+                            value={this.props.user.group}
+                        />
+                    </div>
                 </div>
             </div>
-        </>;
+        );
     }
 }
 
