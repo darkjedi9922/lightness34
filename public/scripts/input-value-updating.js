7c459e8dd6157424b3c95212d72f5fcdee3a3cda
Jed Dark <darkjedi9922@gmail.com>
2020-01-25 10:47:26 +0200

    Remove old unused js script for input value updating.

--- public/scripts/input-value-updating.js
+++
@@ -1,9 +0,0 @@
-function setUpdatingInputValueFromInput(fromId, whereId) {
-    from = document.getElementById(fromId);
-    where = document.getElementById(whereId);
-    from.onchange = function() {
-        value = from.value.split('\\')[2];
-        if (value) where.value = value;
-        else where.value = '';
-    }
-}
