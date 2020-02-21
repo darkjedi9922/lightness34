d6b330332b723a223906c48c498a6526b8488cc4
Jed Dark <darkjedi9922@gmail.com>
2020-02-21 21:19:58 +0200

    Use area chart instead line chart in queries statistics.

--- public/scripts/admin/components/pages/database/queries.tsx
+++ public/scripts/admin/components/pages/database/queries.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Breadcrumbs from '../../common/Breadcrumbs';
 import {
-    LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer
+    AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer
 } from 'recharts';
 import $ from 'jquery';
 import { isNil } from 'lodash';
@@ -37,40 +37,42 @@ class QueriesPage extends React.Component<{}, QueriesPageState> {
     }
 
     public render(): React.ReactNode {
-        return <LoadingContent>
-            {!isNil(this.state.data) && <>
-                <div className="content__header">
-                    <div className="breadcrumbs-wrapper">
-                        <Breadcrumbs items={[
-                            {name: 'Мониторинг'},
-                            {name: 'База данных'},
-                            {name: 'Запросы'}
-                        ]} />
+        return <>
+            <div className="content__header">
+                <div className="breadcrumbs-wrapper">
+                    <Breadcrumbs items={[
+                        { name: 'Мониторинг' },
+                        { name: 'База данных' },
+                        { name: 'Запросы' }
+                    ]} />
+                </div>
+            </div>
+            <LoadingContent>
+                {!isNil(this.state.data) &&
+                    <div className="box chart">
+                        <ResponsiveContainer height={400} width="99%">
+                            <AreaChart
+                                data={this.state.data}
+                                margin={{
+                                    top: 10, right: 30, left: -20, bottom: 10,
+                                }}
+                            >
+                                <XAxis dataKey="time" />
+                                <YAxis />
+                                <Tooltip isAnimationActive={false} />
+                                <Area 
+                                    type="monotone"
+                                    dataKey="count" 
+                                    className="chart__area"
+                                    dot={{ r: 3, className: 'chart__dot' }}
+                                    activeDot={{ r: 4, className: 'chart__dot' }}
+                                />
+                            </AreaChart>
+                        </ResponsiveContainer>
                     </div>
-                </div>
-                <div className="box chart">
-                    <ResponsiveContainer height={400} width="99%">
-                        <LineChart
-                            data={this.state.data}
-                            margin={{
-                                top: 10, right: 30, left: -20, bottom: 10,
-                            }}
-                        >
-                            <XAxis dataKey="time"/>
-                            <YAxis />
-                            <Tooltip isAnimationActive={false} />
-                            <Line 
-                                type="monotone"
-                                dataKey="count" 
-                                className="chart__line"
-                                dot={{ r: 4, className: 'chart__dot' }}
-                                activeDot={{ r: 5, className: 'chart__dot' }}
-                            />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
-            </>}
-        </LoadingContent>;
+                }
+            </LoadingContent>
+        </>;
     }
 }
 
