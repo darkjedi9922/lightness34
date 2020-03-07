4a939ed275a34df1cb5cc266a2768d96cb729264
Jed Dark <darkjedi9922@gmail.com>
2020-03-07 11:24:11 +0200

    Update menu items access accordingly to modules rights.

--- views/blocks/admin/left.php
+++ views/blocks/admin/left.php
@@ -1,150 +1,180 @@
 <?php /** @var frame\views\Block $self */
 
-use engine\users\cash\my_group;
 use engine\users\cash\my_rights;
+use engine\users\Group;
+use engine\users\cash\user_me;
 
-$group = my_group::get();
-$rights = my_rights::get('admin');
+$adminRights = my_rights::get('admin');
+$usersRights = my_rights::get('users');
+$messageRights = my_rights::get('messages');
+$articleRights = my_rights::get('articles');
+$commentsRights = my_rights::get('articles/comments');
+$statsRights = my_rights::get('stat');
+$isIRoot = user_me::get()->group_id === Group::ROOT_ID;
 
-$menu = [
-    'items' => [[
-        'name' => 'Главная',
-        'link' => '/admin/home',
-        'icon' => 'home'
-    ], [
-        'name' => 'Пользователи',
-        'link' => '/admin/users',
-        'icon' => 'user'
-    ], [
+$globalItems = [];
+
+$globalItems[] = [
+    'name' => 'Главная',
+    'link' => '/admin/home',
+    'icon' => 'home'
+];
+
+if ($usersRights->can('see-list')) $globalItems[] = [
+    'name' => 'Пользователи',
+    'link' => '/admin/users',
+    'icon' => 'user'
+];
+
+if ($articleRights->can('see-list')) $globalItems[] = [
+    'name' => 'Статьи',
+    'link' => '/admin/articles',
+    'icon' => 'doc-text'
+];
+
+if ($articleRights->can('see-new-list')) $globalItems[] = [
+    'name' => 'Новое',
+    'icon' => 'rss',
+    'submenu' => [[
         'name' => 'Статьи',
-        'link' => '/admin/articles',
+        'link' => '/admin/new/articles',
         'icon' => 'doc-text'
-    ], [
-        'name' => 'Новое',
-        'icon' => 'rss',
-        'submenu' => [[
-            'name' => 'Статьи',
-            'link' => '/admin/new/articles',
-            'icon' => 'doc-text'
-        ]]
     ]]
 ];
 
-if ($group->id === $group::ROOT_ID) $menu = array_merge_recursive($menu, [
-    'items' => [[
-        'name' => 'Настройки',
-        'icon' => 'cog',
-        'submenu' => [[
-            'name' => 'Общие',
-            'icon' => 'sliders',
-            'link' => '/admin/settings/core'
-        ], [
-            'name' => 'Пользователи',
-            'icon' => 'user',
-            'submenu' => [[
-                'name' => 'Общие',
-                'icon' => 'cog',
-                'link' => '/admin/settings/users'
-            ], [
-                'name' => 'Группы',
-                'icon' => 'id-card-o',
-                'link' => '/admin/users/groups'
-            ], [
-                'name' => 'Пол',
-                'icon' => 'transgender',
-                'link' => '/admin/users/genders'
-            ], [
-                'name' => 'Сообщения',
-                'icon' => 'email',
-                'link' => '/admin/settings/messages'
-            ]]
-        ], [
-            'name' => 'Статьи',
-            'icon' => 'doc-text',
-            'link' => '/admin/settings/articles'
-        ], [
-            'name' => 'Комментарии',
-            'icon' => 'commenting',
-            'link' => '/admin/settings/comments'
-        ], [
-            'name' => 'Мониторинг',
-            'icon' => 'chart-bar',
-            'link' => '/admin/settings/statistics'
-        ], [
-            'name' => 'Админ-панель',
-            'icon' => 'television',
-            'link' => '/admin/settings/admin'
-        ]]
-    ]]
-]);
+$settingsItems = [];
 
-if ($rights->can('see-logs')) $menu = array_merge_recursive($menu, [
-    'items' => [[
-        'name' => 'Мониторинг',
-        'icon' => 'chart-bar',
+if ($isIRoot) $settingsItems[] = [
+    'name' => 'Общие',
+    'icon' => 'sliders',
+    'link' => '/admin/settings/core'
+];
+
+$userSettingsItems = [];
+
+if ($usersRights->can('setup')) $userSettingsItems[] = [
+    'name' => 'Общие',
+    'icon' => 'cog',
+    'link' => '/admin/settings/users'
+];
+if ($isIRoot) $userSettingsItems[] = [
+    'name' => 'Группы',
+    'icon' => 'id-card-o',
+    'link' => '/admin/users/groups'
+];
+if ($usersRights->can('configure-genders')) $userSettingsItems[] = [
+    'name' => 'Пол',
+    'icon' => 'transgender',
+    'link' => '/admin/users/genders'
+];
+if ($messageRights->can('setup')) $userSettingsItems[] = [
+    'name' => 'Сообщения',
+    'icon' => 'email',
+    'link' => '/admin/settings/messages'
+];
+
+if (!empty($userSettingsItems)) $settingsItems[] = [
+    'name' => 'Пользователи',
+    'icon' => 'user',
+    'submenu' => $userSettingsItems
+];
+
+if ($articleRights->can('configure')) $settingsItems[] = [
+    'name' => 'Статьи',
+    'icon' => 'doc-text',
+    'link' => '/admin/settings/articles'
+];
+if ($commentsRights->can('configure')) $settingsItems[] = [
+    'name' => 'Комментарии',
+    'icon' => 'commenting',
+    'link' => '/admin/settings/comments'
+];
+if ($statsRights->can('configure')) $settingsItems[] = [
+    'name' => 'Мониторинг',
+    'icon' => 'chart-bar',
+    'link' => '/admin/settings/statistics'
+];
+if ($isIRoot) $settingsItems[] = [
+    'name' => 'Админ-панель',
+    'icon' => 'television',
+    'link' => '/admin/settings/admin'
+];
+
+if (!empty($settingsItems)) $globalItems[] = [
+    'name' => 'Настройки',
+    'icon' => 'cog',
+    'submenu' => $settingsItems
+];
+
+if ($statsRights->can('see')) $globalItems[] = [
+    'name' => 'Мониторинг',
+    'icon' => 'chart-bar',
+    'submenu' => [[
+        'name' => 'Маршруты',
+        'icon' => 'link',
         'submenu' => [[
-            'name' => 'Маршруты',
-            'icon' => 'link',
-            'submenu' => [[
-                'name' => 'История',
-                'icon' => 'clock',
-                'link' => '/admin/statistics/routes/history'
-            ], [
-                'name' => 'Статистика',
-                'icon' => 'chart-area',
-                'link' => '/admin/statistics/routes/charts'
-            ]]
+            'name' => 'История',
+            'icon' => 'clock',
+            'link' => '/admin/statistics/routes/history'
         ], [
-            'name' => 'События',
-            'icon' => 'flash-1',
-            'link' => '/admin/statistics/events'
-        ], [
-            'name' => 'Модули',
-            'icon' => 'cube',
-            'link' => '/admin/statistics/modules'
-        ], [
-            'name' => 'Виды',
-            'icon' => 'television',
-            'link' => '/admin/statistics/views'
-        ], [
-            'name' => 'Действия',
-            'icon' => 'superpowers',
-            'submenu' => [[
-                'name' => 'История',
-                'icon' => 'clock',
-                'link' => '/admin/statistics/actions/history'
-            ], [
-                'name' => 'Каталог',
-                'icon' => 'folder',
-                'link' => '/admin/statistics/actions/catalog'
-            ]]
-        ], [
-            'name' => 'База данных',
-            'icon' => 'database',
-            'submenu' => [[
-                'name' => 'История',
-                'icon' => 'clock',
-                'link' => '/admin/statistics/database/history'
-            ], [
-                'name' => 'Запросы',
-                'icon' => 'flag',
-                'link' => '/admin/statistics/database/queries'
-            ], [
-                'name' => 'Таблицы',
-                'icon' => 'folder',
-                'link' => '/admin/statistics/database/tables'
-            ]]
-        ], [
-            'name' => 'Кеш',
-            'icon' => 'floppy',
-            'link' => '/admin/statistics/cash'
+            'name' => 'Статистика',
+            'icon' => 'chart-area',
+            'link' => '/admin/statistics/routes/charts'
         ]]
     ], [
-        'name' => 'Лог',
-        'icon' => 'doc-text',
-        'link' => '/admin/log'
+        'name' => 'События',
+        'icon' => 'flash-1',
+        'link' => '/admin/statistics/events'
+    ], [
+        'name' => 'Модули',
+        'icon' => 'cube',
+        'link' => '/admin/statistics/modules'
+    ], [
+        'name' => 'Виды',
+        'icon' => 'television',
+        'link' => '/admin/statistics/views'
+    ], [
+        'name' => 'Действия',
+        'icon' => 'superpowers',
+        'submenu' => [[
+            'name' => 'История',
+            'icon' => 'clock',
+            'link' => '/admin/statistics/actions/history'
+        ], [
+            'name' => 'Каталог',
+            'icon' => 'folder',
+            'link' => '/admin/statistics/actions/catalog'
+        ]]
+    ], [
+        'name' => 'База данных',
+        'icon' => 'database',
+        'submenu' => [[
+            'name' => 'История',
+            'icon' => 'clock',
+            'link' => '/admin/statistics/database/history'
+        ], [
+            'name' => 'Запросы',
+            'icon' => 'flag',
+            'link' => '/admin/statistics/database/queries'
+        ], [
+            'name' => 'Таблицы',
+            'icon' => 'folder',
+            'link' => '/admin/statistics/database/tables'
+        ]]
+    ], [
+        'name' => 'Кеш',
+        'icon' => 'floppy',
+        'link' => '/admin/statistics/cash'
     ]]
-]);
+];
+
+if ($adminRights->can('see-logs')) $globalItems[] = [
+    'name' => 'Лог',
+    'icon' => 'doc-text',
+    'link' => '/admin/log'
+];
+
+$menu = ['items' => $globalItems];
 ?>
 
 <div id="menu" data-props='<?= json_encode($menu, JSON_HEX_AMP) ?>'></div>
