d85803c076875cabc3f8d0b2e8b99742102b8a24
Jed Dark <darkjedi9922@gmail.com>
2020-03-21 20:34:44 +0200

    Make style of Module code more stylized.

--- frame/modules/Module.php
+++ frame/modules/Module.php
@@ -10,31 +10,29 @@ abstract class Module
     /**
      * @param string $name Должно быть уникальным в пределах всего приложения.
      */
-    public function __construct(string $name, Module $parent = null) {
+    public function __construct(string $name, Module $parent = null)
+    {
         $this->name = $parent ? $parent->getName() . '/' . $name : $name;
         $this->parent = $parent;
     }
 
-    /**
-     * Имя является строковым идентификатором каждого модуля.
-     */
-    public function getName(): string {
+    /** Имя является строковым идентификатором каждого модуля. */
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    /**
-     * Id является числовым идентификатором каждого модуля, основанный на имени.
-     */
-    public function getId(): int {
+    /** Id является числовым идентификатором каждого модуля, основанный на имени. */
+    public function getId(): int
+    {
         return crc32($this->name);
     }
 
-    public function getParent(): ?Module {
+    public function getParent(): ?Module
+    {
         return $this->parent;
     }
 
-    /**
-     * Модуль может не иметь прав, тогда нужно вернуть null.
-     */
+    /** Модуль может не иметь прав, тогда нужно вернуть null. */
     public abstract function createRightsDescription(): ?RightsDesc;
 }
