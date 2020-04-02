e6a7eac76ea8301c17ea67a6555ab7a2db17db3c
Jed Dark <darkjedi9922@gmail.com>
2020-04-02 10:30:02 +0300

    Use History component in CashUseHistory.

--- views/pages/api/stats/cash/history.php
+++ views/pages/api/stats/cash/history.php
@@ -1,15 +1,14 @@
-<?php /** @var frame\views\Page $self */
+<?php
 
 use frame\lists\base\IdentityList;
 use engine\statistics\stats\CashRouteStat;
 use engine\statistics\stats\CashValueStat;
 use frame\tools\JsonEncoder;
-use frame\actions\ViewAction;
 use frame\database\Records;
 use frame\lists\iterators\IdentityIterator;
 
 $routes = new IdentityList(CashRouteStat::class, ['id' => 'DESC']);
-$routesProps = [];
+$routes = [];
 foreach ($routes as $route) {
     /** @var CashRouteStat $route */
     $cashValues = [];
@@ -29,24 +28,11 @@ foreach ($routes as $route) {
             'calls' => $cashValue->call_count
         ];
     }
-    $routesProps[] = [
+    $routes[] = [
         'route' => $route->route,
         'values' => $cashValues,
         'time' => date('d.m.Y H:i', $route->time)
     ];
 }
-$historyRouteCount = count($routesProps);
-$historyProps = JsonEncoder::forHtmlAttribute([
-    'routes' => $routesProps
-]);
-?>
 
-<div class="content__header">
-    <div class="breadcrumbs">
-        <span class="breadcrumbs__item">Мониторинг</span>
-        <span class="breadcrumbs__divisor"></span>
-        <span class="breadcrumbs__item breadcrumbs__item--current">Кеш</span>
-    </div>
-</div>
-<span class="content__title">История вызовов (<?= $historyRouteCount ?>)</span>
-<div id="cash-use-history" data-props="<?= $historyProps ?>"></div>
+echo JsonEncoder::forViewText($routes);
