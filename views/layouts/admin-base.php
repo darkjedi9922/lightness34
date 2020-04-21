af6a6b0dad29bf0daa7723f3196131da76db2832
Jed Dark <darkjedi9922@gmail.com>
2020-04-21 20:12:24 +0300

    Stylize error pages.

--- views/layouts/admin-base.php
+++ views/layouts/admin-base.php
@@ -1,10 +1,8 @@
 <?php /** @var frame\views\Layout $self */
 
-use frame\stdlib\cash\config;
 use engine\admin\Auth;
 use frame\tools\Init;
 use frame\views\Page;
-use function lightlib\versionify;
 
 Init::accessRight('admin', 'enter');
 
@@ -15,20 +13,6 @@ if (!$auth->isLogged() && !$self->hasChild('pages/admin')) {
     (new Page('admin'))->show();
     return;
 }
-
-$config = config::get('core');
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-    <link rel="icon" href="<?= versionify('public/favicon.ico') ?>" type="image/x-icon">
-    <link rel="stylesheet" href="<?= versionify('public/build/admin.css') ?>" type="text/css">
-    <title><?= $config->{'site.name'} ?></title>
-</head>
-<body>
-    <?php $self->loadChild()->show() ?>
-    <script src="<?= versionify('public/build/admin.js') ?>"></script>
-</body>
-</html>
+<?php $self->loadChild()->show() ?>
