09245b7400d05666ba441ffd5bf3db9c4d86a32a
Jed Dark <darkjedi9922@gmail.com>
2020-05-06 10:19:17 +0300

    Update new articles page design.

--- views/pages/admin/new/articles.php
+++ views/pages/admin/new/articles.php
@@ -2,42 +2,45 @@
 
 use frame\stdlib\cash\pagenumber;
 use engine\articles\NewArticlePagedList;
+use engine\articles\Article;
 use engine\users\User;
-use function lightlib\shorten;
 use frame\tools\Init;
+use frame\tools\JsonEncoder;
+use function lightlib\shorten;
 
 Init::accessRight('articles', 'see-new-list');
 $pagenumber = pagenumber::get();
 $articles = new NewArticlePagedList($pagenumber);
+
+$tableProps = ['items' => []];
+foreach ($articles as $article) {
+    /** @var Article $article */
+    $author = User::selectIdentity($article->author_id);
+    $item = [
+        'id' => $article->id,
+        'title' => shorten($article->title, 80, '...'),
+        'author' => $author->login,
+        'date' => date('d.m.Y H:i', $article->date)
+    ];
+    $tableProps['items'][] = $item;
+}
+$tableProps = JsonEncoder::forHtmlAttribute($tableProps);
 ?>
 
 <div class="content__header">
     <div class="breadcrumbs">
         <span class="breadcrumbs__item">Новое</span>
         <span class="breadcrumbs__divisor"></span>
-        <span class="breadcrumbs__item breadcrumbs__item--current">Статьи</span>
+        <span class="breadcrumbs__item breadcrumbs__item--current">
+            Статьи (<?= $articles->countOnPage() ?>)
+        </span>
+    </div>
+    <div class="actions">
+        <?php if ($articles->getPager()->countPages() > 1) : ?>
+            <div class="actions__item">
+                <?php $articles->getPager()->show('admin') ?>
+            </div>
+        <?php endif ?>
     </div>
 </div>
-<div class="box">
-    <?php if ($articles->countAll() == 0) : ?>Новых статей нет
-    <?php else : ?>
-        <table width="100%">
-            <tr>
-                <td><b>ID</b></td>
-                <td><b>Название</b></td>
-                <td><b>Автор</b></td>
-                <td><b>Дата</b></td>
-            </tr>
-            <?php foreach ($articles as $article): /** @var engine\articles\Article $article */ ?>
-            <?php $author = User::selectIdentity($article->author_id) ?>
-                <tr>
-                    <td><?= $article->id ?></td>
-                    <td><a href="/admin/article?id=<?= $article->id ?>"><?= shorten($article->title, 80, '...') ?></a></td>
-                    <td><a href="/admin/users/profile/<?= $author->login ?>"><?= $author->login ?></a></td>
-                    <td><?= date('d.m.Y H:i', $article->date) ?></td>
-                </tr>
-            <?php endforeach ?>
-        </table>
-        <?php $articles->getPager()->show('admin') ?>
-    <?php endif ?>
-</div>
+<div id="articles" data-props="<?= $tableProps ?>"></div>
