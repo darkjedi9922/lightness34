b9ce16c47f8f8cf98d169d1673f19f36e90752ba
Jed Dark <darkjedi9922@gmail.com>
2020-05-09 18:49:19 +0300

    Fix radio React component.

--- public/scripts/admin/components/form/FormRadio.tsx
+++ public/scripts/admin/components/form/FormRadio.tsx
@@ -6,7 +6,21 @@ interface FormRadioProps {
     field: RadioField
 }
 
-class FormRadio extends React.Component<FormRadioProps> {
+interface FormRadioState {
+    value: string
+}
+
+class FormRadio extends React.Component<FormRadioProps, FormRadioState> {
+    public constructor(props: FormRadioProps) {
+        super(props);
+        this.state = { value: props.field.currentValue };
+    }
+
+    public UNSAFE_componentWillReceiveProps(nextProps: FormRadioProps) {
+        if (this.props.field.currentValue !== nextProps.field.currentValue)
+            this.setState({ value: nextProps.field.currentValue });
+    }
+
     public render(): React.ReactNode {
         return (
             <div className="radio">
@@ -17,19 +31,23 @@ class FormRadio extends React.Component<FormRadioProps> {
                                 type="radio"
                                 name={this.props.field.name}
                                 value={value.value}
-                                defaultChecked={this.props.field.currentValue === value.value}
+                                defaultChecked={this.state.value === value.value}
                                 className="radio__input"
-                                onChange={this.props.field.onChange}
+                                onChange={(event) => {
+                                    this.setState({ value: event.target.value });
+                                    this.props.field.onChange && this.props.field.onChange(event);
+                                }}
                             />
                             <i className={classNames([
                                 'radio__mark',
-                                {'radio__mark--checked': this.props.field.currentValue === value.value},
-                                {'radio__mark--unchecked': this.props.field.currentValue !== value.value}
+                                {'radio__mark--checked': this.state.value === value.value},
+                                {'radio__mark--unchecked': this.state.value !== value.value}
                             ])}></i>
                         </label>
                         <span className="radio__label">{value.label}</span>
                     </div>
                 ))}
+                
             </div>
         )
     }
