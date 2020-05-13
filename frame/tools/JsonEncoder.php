0348272af90193e30bc1252ea55e82c8fd840dea
Jed Dark <darkjedi9922@gmail.com>
2020-05-13 19:19:53 +0300

    Ignore bad UTF8 symbols in JsonEncoder and add an error check.

--- frame/tools/JsonEncoder.php
+++ frame/tools/JsonEncoder.php
@@ -4,11 +4,20 @@ class JsonEncoder
 {
     public static function toValidJson($value, bool $pretty = false): string
     {
-        return json_encode(
+        $result = json_encode(
             $value,
-            JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_UNESCAPED_SLASHES
-                | ($pretty ? JSON_PRETTY_PRINT : 0)
+            JSON_HEX_AMP 
+            | JSON_HEX_APOS 
+            | JSON_HEX_QUOT 
+            | JSON_UNESCAPED_SLASHES
+            | JSON_INVALID_UTF8_IGNORE
+            | ($pretty ? JSON_PRETTY_PRINT : 0)
         );
+
+        if (($error = json_last_error()) !== JSON_ERROR_NONE)
+            throw new \Exception('Json error "' . json_last_error_msg() . '"', $error);
+
+        return $result;
     }
 
     public static function forViewText($value, bool $pretty = false): string
