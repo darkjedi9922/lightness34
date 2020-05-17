1292ff3d7859069097dbbfbdc3d4ac88ba77e173
Jed Dark <darkjedi9922@gmail.com>
2020-05-17 16:03:37 +0300

    Add possibility to create a file in File class without emptying it.

--- frame/tools/files/File.php
+++ frame/tools/files/File.php
@@ -12,27 +12,35 @@ class File
     }
 
     /**
-     * Создает новый файл по указанному пути. Если такой уже был, он стирается.
+     * Создает новый файл по указанному пути.
      * @param string $mode See fopen() mode.
      * @param bool $return Если true, создаст экземпляр этого класса для создаваемого
      * файла и вернет его.
+     * @param bool $doEmpty Стирать ли содержимое файла, если он уже существует.
      */
-    public static function create(string $path, bool $return = false): ?self
-    {
-        $handle = fopen($path, 'w');
+    public static function create(
+        string $path,
+        bool $return = false,
+        bool $doEmpty = true
+    ): ?self {
+        $handle = fopen($path, $doEmpty ? 'w' : 'a');
         fclose($handle);
         return $return ? new self($path) : null;
     }
 
     /**
      * Аналогично File::create(), но также создает директории в пути к файлу, если
-     * они не существуют. Если файл уже создан, его содержимое будет очищено.
+     * они не существуют. Если файл уже создан и параметр doEmpty указан как true,
+     * его содержимое будет очищено.
      */
-    public static function createFullPath(string $path, bool $return = false): ?self
-    {
+    public static function createFullPath(
+        string $path, 
+        bool $return = false,
+        bool $doEmpty = true
+    ): ?self {
         $dir = implode('/', explode('/', $path, -1));
         if ($dir !== '' && !file_exists($dir)) Directory::createRecursive($dir);
-        return self::create($path, $return);
+        return self::create($path, $return, $doEmpty);
     }
 
     public static function delete(string $path)
