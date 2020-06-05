143260d8e04fd12e240b43b3f77d2268c1e7ba9b
Jed Dark <darkjedi9922@gmail.com>
2020-06-05 16:34:05 +0300

    Make Alias support dynamic routing.

--- frame/views/Alias.php
+++ frame/views/Alias.php
@@ -1,22 +1,72 @@
 <?php namespace frame\views;
 
-class Alias extends View
+class _AliasView extends View
 {
-    public static function resolveAlias(string $alias): ?string
-    {
-        $view = static::find($alias);
-        if (!$view) return null;
-        $alias = new static($alias);
-        return $alias->getHtml();
-    }
-
     public static function getExtensions(): array
     {
         return ['php'];
     }
-
     public static function getNamespace(): string
     {
         return 'aliases';
+    }
+};
+
+class Alias
+{
+    private $alias;
+    private $contents;
+    private $args;
+
+    /**
+     * @return static|null
+     */
+    public static function resolveAlias(string $alias)
+    {
+        $viewfile = _AliasView::find($alias);
+        if ($viewfile) return static::createAlias($alias, $alias, []);
+
+        $dynamicRouter = new ViewDynamicRouter(_AliasView::class);
+        $route = $dynamicRouter->findRealRoute($alias);
+        if ($route) return static::createAlias($alias, $route->url, $route->args);
+
+        return null;
+    }
+
+    /**
+     * @return static
+     */
+    private static function createAlias(string $alias, string $viewname, array $args)
+    {
+        $view = new _AliasView($viewname);
+        $aliasObject = new static($alias, $view->getHtml(), $args);
+        return $aliasObject;
+    }
+
+    public function __construct(string $alias, string $contents, array $dynamicArgs)
+    {
+        $this->alias = $alias;
+        $this->contents = $contents;
+        $this->args = $dynamicArgs;
+    }
+
+    public function getAlias(): string
+    {
+        return $this->alias;
+    }
+
+    public function getContents(): string
+    {
+        return $this->contents;
+    }
+
+    public function getDynamicArgs(): array
+    {
+        return $this->args;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getContents();
     }
 }
--- tests/views/AliasTest.php
+++ tests/views/AliasTest.php
@@ -1,6 +1,8 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use frame\core\Core;
+use frame\route\Router;
+use frame\stdlib\drivers\route\UrlRouter;
 use frame\views\Alias;
 use frame\views\ViewRouter;
 use tests\views\stubs\ViewRouterStub;
@@ -10,6 +12,7 @@ class AliasTest extends TestCase
     public static function setUpBeforeClass(): void
     {
         $app = new Core([
+            Router::class => UrlRouter::class,
             ViewRouter::class => ViewRouterStub::class
         ]);
     }
@@ -18,11 +21,23 @@ class AliasTest extends TestCase
     {
         $expectedAlias = 'some/alias/string';
         $actualAlias = Alias::resolveAlias('some-alias');
-        $this->assertEquals($expectedAlias, $actualAlias);
+        $this->assertEquals($expectedAlias, (string) $actualAlias);
     }
 
     public function testReturnsNullIfAnAliasIsNotFound()
     {
         $this->assertNull(Alias::resolveAlias('some-non-existence-alias'));
+    }
+
+    public function testSupportsDynamicRouting()
+    {
+        $expectedAlias = 'some/dynamic/alias';
+        $expectedDynamicArgs = ['dynamic-parameter'];
+        
+        $actualAlias = Alias::resolveAlias("dynamic-parameter/dynamic-alias");
+        
+        $this->assertNotNull($actualAlias);
+        $this->assertEquals($expectedAlias, $actualAlias->getContents());
+        $this->assertEquals($expectedDynamicArgs, $actualAlias->getDynamicArgs());
     }
 }
